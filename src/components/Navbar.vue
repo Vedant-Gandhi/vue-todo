3f8756aa8c18f9c3edf5c4786d9b4ae4e46e2134
Vedant-Gandhi <vedantgandhipersonal@gmail.com>
2025-04-08 11:55:33 +0530

    feat: add router logic to hide add task button.

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,9 +1,18 @@
-<script lang="ts">
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useRoute } from 'vue-router' // Add this import
 
+const route = useRoute()
+
+console.log(route.path) // Add this line to check if the component is mounted
+
+const isCreateTaskPage = computed(() => {
+  return route.path === '/create-task'
+})
 </script>
 <template>
 <nav class="w-full px-3 py-5 bg-gray-900 flex items-center flex-row-reverse">
-    <RouterLink to="/create-task"><button class="bg-blue-500 w-10 aspect-square rounded-full font-extrabold text-lg cursor-pointer hover:bg-blue-400 transition-colors duration-300 outline-none">+</button></RouterLink>
+    <RouterLink v-show="!isCreateTaskPage" to="/create-task"><button class="bg-blue-500 w-10 aspect-square rounded-full font-extrabold text-lg cursor-pointer hover:bg-blue-400 transition-colors duration-300 outline-none">+</button></RouterLink>
     <h3 class="mr-auto font-semibold text-lg select-none">Vue TODO</h3>
 </nav> 
 </template>
