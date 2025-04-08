045d8cf48d7fc3893b9c348db833f3172ba41b58
Vedant-Gandhi <vedantgandhipersonal@gmail.com>
2025-04-08 11:36:48 +0530

    feat: add the create task route button.

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,9 @@
 <script lang="ts">
+
 </script>
 <template>
-<nav class="w-full px-3 py-5 bg-gray-900">
-    
+<nav class="w-full px-3 py-5 bg-gray-900 flex items-center flex-row-reverse">
+    <RouterLink to="/create-task"><button class="bg-blue-500 w-10 aspect-square rounded-full font-extrabold text-lg cursor-pointer hover:bg-blue-400 transition-colors duration-300 outline-none">+</button></RouterLink>
+    <h3 class="mr-auto font-semibold text-lg select-none">Vue TODO</h3>
 </nav> 
 </template>
